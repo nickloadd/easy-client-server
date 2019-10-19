bf3041ff5cd8c5ff9af5ccb28ae2539cdd77a924
nickload <nickload1613@gmail.com>
2019-10-19 22:07:23 +0700

    add ASPController

---
+++ ASPController/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace ASPController.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Default");
+        }
+
+        [HttpPost]
+        public async Task<string> Hello(string name)
+        {
+            string userName = await Task.Factory.StartNew(()=>  JsonConvert.DeserializeObject<string>(name));
+            return await Task.Factory.StartNew(() => JsonConvert.SerializeObject(string.Format("Hello {0}", userName)));
+        }
+    }
+}
